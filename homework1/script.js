d6712764811bc8b1b2ea336a0705e0ba90c83f07
Marianna148 <99441179+Marianna148@users.noreply.github.com>
2022-05-31 22:04:21 +0200

    Add files via upload

---
+++ homework1/script.js
@@ -0,0 +1,24 @@
+console.log("**************");
+
+x = Math.PI *= 100;
+y = 10;
+z = 20;
+
+sum = x + z;
+
+mult = x * z;
+
+console.log("Обьем цилиндра с площадью основы S = " , x , ", радиусом R = ", y , "и высотой H =", z, "равен:");
+
+console.log("--------------------");
+
+console.log("V =", mult);
+
+console.log("-------------------");
+
+console.log("end.");
+
+
+
+
+
